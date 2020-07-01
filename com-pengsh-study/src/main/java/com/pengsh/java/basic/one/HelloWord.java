9ee46c8bb1b16866980daf4ad4065bfcad8a9d4d
pengsh <pengsh@fxiaoke.com>
2020-07-01 16:05:18 +0800

    写了一些排序算法

--- com-pengsh-study/src/main/java/com/pengsh/java/basic/one/HelloWord.java
+++ com-pengsh-study/src/main/java/com/pengsh/java/basic/one/HelloWord.java
@@ -6,6 +6,29 @@ package com.pengsh.java.basic.one;
  */
 public class HelloWord {
     public static void main(String[] args) {
+        maopao();
+        System.out.println(1>1);
+    }
 
+    public static void maopao() {
+        int[] a = new int[]{12, 23, 1, 121, 1341, 2, 3, 12, 341, 31};
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    int tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+        for (int b : a) {
+            System.out.println(b);
+        }
+    }
+    public static void kuaipai(){
+        int[] a = new int[]{12, 23, 1, 121, 1341, 2, 3, 12, 341, 31};
+        for(int i = 0; i < a.length; i++){
+
+        }
     }
 }
