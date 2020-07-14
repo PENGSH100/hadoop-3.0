d36b3a1fcd06e621db4810a4c201c9464f800982
pengsh <pengsh@fxiaoke.com>
2020-07-14 10:18:03 +0800

    写了一些排序算法

--- com-pengsh-study/src/main/java/com/pengsh/java/basic/one/SortUtils.java
+++ com-pengsh-study/src/main/java/com/pengsh/java/basic/one/SortUtils.java
@@ -72,6 +72,12 @@ public class SortUtils {
         }
         return a;
     }
+
+    /**
+     * 快速排序 先找一个基准 然后在进行拍需要
+     * @param a
+     * @return
+     */
     public static int[] quickSort(int[] a){
         if(a==null||a.length<2){
             return a;
