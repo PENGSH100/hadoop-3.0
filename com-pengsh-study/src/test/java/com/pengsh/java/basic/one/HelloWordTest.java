ca5a18fa5444f81adff74477b73f960e3ba9fd2d
pengsh <15845633608@163.com>
2020-10-06 21:00:38 +0800

    增加测试类的依赖

---
+++ com-pengsh-study/src/test/java/com/pengsh/java/basic/one/HelloWordTest.java
@@ -0,0 +1,15 @@
+package com.pengsh.java.basic.one;
+
+
+import org.junit.Test;
+
+/**
+ * @author pengsh
+ * @date 2020/10/6 20:54
+ */
+public class HelloWordTest {
+    @Test
+    public void test() {
+        System.out.println("ceshiyxia");
+    }
+}
