882f1f9f2028aff3709f65daa98c0f7251f8dc7c
pengshuai <pengsh@fxiaoke.com>
2020-10-25 10:25:53 +0800

    写了设计模式

--- com-pengsh-study/src/main/java/com/pengsh/java/basic/one/readFile.java
+++ com-pengsh-study/src/main/java/com/pengsh/java/basic/one/readFile.java
@@ -1,9 +1,40 @@
 package com.pengsh.java.basic.one;
 
+import static com.sun.beans.finder.ClassFinder.findClass;
+
 /**
  * @author pengsh
  * @date 2020/7/16 23:00
  */
 public class readFile {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
 
+    public static void main(String[] args) {
+        ClassLoader c=new ClassLoader() {
+            @Override
+            public Class<?> loadClass(String name) throws ClassNotFoundException {
+                Class cc=findClass(name);
+                if (cc==null){
+
+                }
+                return super.loadClass(name);
+            }
+        };
+    }
+    protected synchronized Class<?> loadClass(String name,boolean resolve)throws ClassNotFoundException{
+        Class cc=findClass(name);
+        if(cc==null){
+            try {
+
+            }catch (Exception e){
+
+            }
+        }
+
+
+        return cc;
+    }
 }
--- com-pengsh-study/src/test/java/com/pengsh/java/basic/one/HelloWordTest.java
+++ com-pengsh-study/src/test/java/com/pengsh/java/basic/one/HelloWordTest.java
@@ -1,6 +1,8 @@
 package com.pengsh.java.basic.one;
 
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -8,7 +10,16 @@ import org.junit.Test;
  * @date 2020/10/6 20:54
  */
 public class HelloWordTest {
+    @BeforeClass
+    public static void init(){
+        System.out.println("haha");
+        System.setProperty("process.profile", "fstest");
 
+    }
+    @Before
+    public void before(){
+        System.out.println("haha");
+    }
     @Test
     public void test() {
         System.out.println("ceshiyxia");
